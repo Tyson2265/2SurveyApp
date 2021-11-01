189c31e19d93f3560d01035dd7bf497962e16396
Tyson2265 <76685892+Tyson2265@users.noreply.github.com>
2021-11-01 22:38:20 +0200

    Update code.php

--- code.php
+++ code.php
@@ -25,7 +25,7 @@ if(isset($_POST['submit']))
     if($query_run)
     {
         $_SESSION['status'] = "Inserted Successfully";
-        header("Location: Survey.php");
+        header("Location: index.php");
     }
     else{
         $_SESSION['status'] = "Not Inserted Successfully";
